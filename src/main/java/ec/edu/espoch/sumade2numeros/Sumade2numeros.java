4d32ecd5286bf5626f907a79ed35a46d58bafb99
anthonypulupa <anthonypulupa2@gmail.com>
2024-10-30 10:20:57 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/sumade2numeros/Sumade2numeros.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.sumade2numeros;
+
+/**
+ *
+ * @author anthony
+ */
+public class Sumade2numeros {
+
+    public static void main(String[] args) {
+       int num1=4;
+         int num2=2;
+         int result= num1+num2;
+         System.out.println("el resultado es :"+result);
+    }
+}
